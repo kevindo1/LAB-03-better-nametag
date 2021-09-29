12dd6fb33de3bc8ddeae64c36469180621db1ba7
Kevin Do <kevind0501@gmail.com>
2021-09-29 11:54:42 -0700

    Added interactive name input using javascript

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // import functions and grab DOM elements
+const nameInput = document.getElementById('name');
+const button = document.getElementById('generate');
+const firstName = document.getElementById('first-name');
 
 // initialize global state
 
@@ -6,3 +9,8 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+button.addEventListener('click', ()=>{
+  let newName = nameInput.value;
+  firstName.textContent = newName;
+});
